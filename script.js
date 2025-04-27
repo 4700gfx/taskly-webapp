952b694a510c59d80f0429c2530f34f559bb9132
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-04-27 12:32:12 -0400

    Adding Variables and Event Listeners

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 //VARIABLES FOR APPLICATION 
 
+//Main To Do Component Variables
+const homePanel = document.querySelector('home-panel');
+const filteredList = document.querySelector('filtered-list')
+const taskContainer = document.querySelector('task-container')
+const taskList = document.querySelector('task-list')
+const taskItem = document.querySelector('task-item')
+
+//Buttons Variables 
+const addTaskButton = document.querySelector('.add-task-button');
+const pauseButton = document.querySelector('.pause-button');
+const playButton = document.querySelector('.play-button');
+const stopButton = document.querySelector('.stop-button');
+
+
+addTaskButton.addEventListener('click', function(){
+  console.log('Adding Task')
+})
+
+playButton.addEventListener('click', function(){
+  console.log('Adding Task')
+})
+
+
+pauseButton.addEventListener('click', function(){
+  console.log('Adding Task')
+})
+
+
+stopButton.addEventListener('click', function(){
+  console.log('Adding Task')
+})
 
