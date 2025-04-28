394cec969d61012f1bf4e532acf8db84cf62398d
47gfx <117771075+4700gfx@users.noreply.github.com>
2025-04-28 12:54:05 -0400

    Adding Add Task Functionality

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
 //VARIABLES FOR APPLICATION 
 
 //Main To Do Component Variables
-const homePanel = document.querySelector('home-panel');
-const filteredList = document.querySelector('filtered-list')
-const taskContainer = document.querySelector('task-container')
-const taskList = document.querySelector('task-list')
-const taskItem = document.querySelector('task-item')
+const homePanel = document.querySelector('.home-panel');
+const filteredList = document.querySelector('.filtered-list')
+const taskContainer = document.querySelector('.task-container')
+const taskList = document.querySelector('.task-list')
+const taskItem = document.querySelector('.task-item')
 
 //Buttons Variables 
+let input = document.querySelector('.task-input')
 const addTaskButton = document.querySelector('.add-task-button');
 const pauseButton = document.querySelector('.pause-button');
 const playButton = document.querySelector('.play-button');
 const stopButton = document.querySelector('.stop-button');
 
 
+
+
+
+
+
+
+
+
+
+
+
+//Event Listeners
 addTaskButton.addEventListener('click', function(){
-  console.log('Adding Task')
+  //Caputures Value of Input Bar 
+  let taskName = input.value 
+
+  const newTask = document.createElement('li')
+  newTask.textContent = taskName;
+  document.querySelector('.list-container').appendChild(newTask)
+  input.value = " ";
+  console.log(taskName)
+
+  
 })
 
 playButton.addEventListener('click', function(){
-  console.log('Adding Task')
+  console.log('Starting Clock')
 })
 
 
 pauseButton.addEventListener('click', function(){
-  console.log('Adding Task')
+  console.log('Pausing Clock')
 })
 
 
 stopButton.addEventListener('click', function(){
-  console.log('Adding Task')
+  console.log('Stopping Clock')
 })
 
